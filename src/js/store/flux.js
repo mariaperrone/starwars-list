3c3625bf50ae3bf06cb1300d262341ec64b1d406
mariaperrone <perronemar148@gmail.com>
2021-09-25 00:54:54 +0000

    in progress

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,4 +1,6 @@
 const getState = ({ getStore, getActions, setStore }) => {
+	const urlPers = "https://www.swapi.tech/api/people/";
+	const urlPlan = "https://www.swapi.tech/api/planets/";
 	return {
 		store: {
 			demo: [
@@ -12,7 +14,9 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			listaPersonajes: [],
+			listaPlanetas: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -20,9 +24,41 @@ const getState = ({ getStore, getActions, setStore }) => {
 				getActions().changeColor(0, "green");
 			},
 			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+				fetch(urlPers, {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(resp => {
+						return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+					})
+					.then(data => {
+						//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+						console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+					})
+					.catch(error => {
+						//manejo de errores
+						alert(error);
+					});
+
+				fetch(urlPlan, {
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(resp => {
+						return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+					})
+					.then(data => {
+						//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+						console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+					})
+					.catch(error => {
+						//manejo de errores
+						alert(error);
+					});
 			},
 			changeColor: (index, color) => {
 				//get the store
